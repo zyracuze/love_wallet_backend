414d2ca089d00da6ac1fb4d95512ffa2d55aedb4
eddie <seksan404@gmail.com>
2017-09-30 16:47:54 +0700

    fix api route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
-app.use('/', routes)
+app.use('/api/', routes)
 app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
 });
