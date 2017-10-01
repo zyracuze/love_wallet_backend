375f6d949cd94d95dcc6d5134e4e4f35e12ea85c
Somkiat Puisungnoen <somkiat.p@gmail.com>
2017-10-01 11:28:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker container rm -f backend
 docker image build -t backend .
-#docker container run -d --name backend -p 3000:3000 backend
+docker container run -d --name backend -p 3000:3000 backend
