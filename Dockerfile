4ed62ad6fd4e28c8997d94840cb27b449938ba36
up1 <you@example.com>
2017-10-01 07:22:56 +0000

    Add mongodb and remove step to build image

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:6.11.3-alpine
 RUN mkdir -p /home/web
-COPY *.js /home/web/
-COPY modules /home/web/modules/
+#COPY *.js /home/web/
+#COPY modules /home/web/modules/
 COPY *.json /home/web/
 WORKDIR /home/web
 RUN npm install
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 docker container rm -f backend
+docker container rm -f mongo
+
+docker container run -d --name mongo mongo:3.5.13-jessie
+
 docker image build -t backend .
-docker container run -d --name backend -p 3000:3000 backend
+docker container run -d --name backend --link mongo -p 3000:3000 backend
